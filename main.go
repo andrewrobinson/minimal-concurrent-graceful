ea2731e9f55d03db98d811f4bc5f7a6ce5f43ba7
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-08-26 23:27:16 +0200

    adding in examples from the book

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ func main() {
 
 	// parentSignalsSingleChildDone()
 	// parentSignalsSingleChildDone2()
-	errorHandling()
+	// errorHandling()
+	// pipelinesBatchStyle()
+	// pipelinesStreamStyle()
+	// pipelinesWithChannels()
+	// repeatAndTakeGenerators()
+	repeatFnGenerator()
+	// fanInFanOutPrimes()
 
 	// fiveCycles()
 
@@ -26,7 +32,322 @@ func main() {
 
 }
 
+func fanInFanOutPrimes() {
+
+	//this is missing impls for primeFinder and toInt
+
+	// take := func(
+	// 	done <-chan interface{},
+	// 	valueStream <-chan interface{},
+	// 	num int,
+	// ) <-chan interface{} {
+	// 	takeStream := make(chan interface{})
+	// 	go func() {
+	// 		defer close(takeStream)
+	// 		for i := 0; i < num; i++ {
+	// 			select {
+	// 			case <-done:
+	// 				return
+	// 			case takeStream <- <-valueStream:
+	// 			}
+	// 		}
+	// 	}()
+	// 	return takeStream
+	// }
+
+	// repeatFn := func(
+	// 	done <-chan interface{},
+	// 	fn func() interface{},
+	// ) <-chan interface{} {
+	// 	valueStream := make(chan interface{})
+	// 	go func() {
+	// 		defer close(valueStream)
+	// 		for {
+	// 			select {
+	// 			case <-done:
+	// 				return
+	// 			case valueStream <- fn():
+	// 			}
+	// 		}
+	// 	}()
+	// 	return valueStream
+	// }
+
+	// fanIn := func(
+	// 	done <-chan interface{},
+	// 	channels ...<-chan interface{},
+	// ) <-chan interface{} {
+	// 	var wg sync.WaitGroup
+	// 	multiplexedStream := make(chan interface{})
+
+	// 	multiplex := func(c <-chan interface{}) {
+	// 		defer wg.Done()
+	// 		for i := range c {
+	// 			select {
+	// 			case <-done:
+	// 				return
+	// 			case multiplexedStream <- i:
+	// 			}
+	// 		}
+	// 	}
+
+	// 	// Select from all the channels
+	// 	wg.Add(len(channels))
+	// 	for _, c := range channels {
+	// 		go multiplex(c)
+	// 	}
+
+	// 	// Wait for all the reads to complete
+	// 	go func() {
+	// 		wg.Wait()
+	// 		close(multiplexedStream)
+	// 	}()
+
+	// 	return multiplexedStream
+	// }
+
+	// done := make(chan interface{})
+	// defer close(done)
+
+	// start := time.Now()
+
+	// rand := func() interface{} { return rand.Intn(50000000) }
+
+	// randIntStream := toInt(done, repeatFn(done, rand))
+
+	// numFinders := runtime.NumCPU()
+	// fmt.Printf("Spinning up %d prime finders.\n", numFinders)
+	// finders := make([]<-chan interface{}, numFinders)
+	// fmt.Println("Primes:")
+	// for i := 0; i < numFinders; i++ {
+	// 	finders[i] = primeFinder(done, randIntStream)
+	// }
+
+	// for prime := range take(done, fanIn(done, finders...), 10) {
+	// 	fmt.Printf("\t%d\n", prime)
+	// }
+
+	// fmt.Printf("Search took: %v", time.Since(start))
+
+}
+
+func repeatFnGenerator() {
+
+	//an infinite channel of random integers generated on an as-needed basis!
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	repeatFn := func(
+		done <-chan interface{},
+		fn func() interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	rand := func() interface{} { return rand.Int() }
+
+	for num := range take(done, repeatFn(done, rand), 10) {
+		fmt.Println(num)
+	}
+}
+
+func repeatAndTakeGenerators() {
+
+	repeat := func(
+		done <-chan interface{},
+		values ...interface{},
+	) <-chan interface{} {
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				for _, v := range values {
+					select {
+					case <-done:
+						return
+					case valueStream <- v:
+					}
+				}
+			}
+		}()
+		return valueStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%v ", num)
+	}
+
+}
+
+func pipelinesWithChannels() {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStream <- i:
+				}
+			}
+		}()
+		return intStream
+	}
+
+	multiply := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		multiplier int,
+	) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	add := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		additive int,
+	) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	intStream := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
+	}
+}
+
+func pipelinesStreamStyle() {
+
+	multiply := func(value, multiplier int) int {
+		return value * multiplier
+	}
+
+	add := func(value, additive int) int {
+		return value + additive
+	}
+
+	ints := []int{1, 2, 3, 4}
+	for _, v := range ints {
+		fmt.Println(multiply(add(multiply(v, 2), 1), 2))
+	}
+
+}
+
+func pipelinesBatchStyle() {
+
+	multiply := func(values []int, multiplier int) []int {
+		multipliedValues := make([]int, len(values))
+		for i, v := range values {
+			multipliedValues[i] = v * multiplier
+		}
+		return multipliedValues
+	}
+
+	add := func(values []int, additive int) []int {
+		addedValues := make([]int, len(values))
+		for i, v := range values {
+			addedValues[i] = v + additive
+		}
+		return addedValues
+	}
+
+	ints := []int{1, 2, 3, 4}
+	for _, v := range add(multiply(ints, 2), 1) {
+		fmt.Println(v)
+	}
+
+	ints = []int{1, 2, 3, 4}
+	for _, v := range multiply(add(multiply(ints, 2), 1), 2) {
+		fmt.Println(v)
+	}
+
+}
+
 func errorHandling() {
+
+	//this actually makes concurrent http requests?
+	//can I achieve 10/100 with this code?
 
 	type Result struct {
 		Error    error
@@ -54,10 +375,16 @@ func errorHandling() {
 	done := make(chan interface{})
 	defer close(done)
 
-	urls := []string{"https://www.google.com", "https://badhost"}
+	errCount := 0
+	urls := []string{"a", "https://www.google.com", "https://badhost", "b", "c", "d"}
 	for result := range checkStatus(done, urls...) {
 		if result.Error != nil {
-			fmt.Printf("error: %v", result.Error)
+			fmt.Printf("error: %v\n", result.Error)
+			errCount++
+			if errCount >= 3 {
+				fmt.Println("Too many errors, breaking!")
+				break
+			}
 			continue
 		}
 		fmt.Printf("Response: %v\n", result.Response.Status)
